91671f4f8d8270ddc653562ce4de1b55b5ff2cd3
Sakshi-Mandyal <mandyalsakshi17@gmail.com>
2024-06-09 12:21:15 +0530

    DataTypes and Variables

---
+++ ex_05062024/Lab004.py
@@ -0,0 +1,19 @@
+# program to find max in two numbers
+max(10, 20)
+print(max(10, 20))
+print(min(10, 20))
+
+# Data Types and Variables
+age = 50
+print(type(age))
+age = "Fifty"
+print(type(age))
+
+name = "Anil"
+print(type(name))
+
+pi = 3.14
+print(type(pi))
+
+isMale = True
+print(type(isMale))
