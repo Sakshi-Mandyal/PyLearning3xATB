79cfaec7e74b78534ef4c01389d44bea18477880
Sakshi-Mandyal <mandyalsakshi17@gmail.com>
2024-06-10 22:17:29 +0530

    Task 1 and 2

---
+++ Task_10062024/Task01.py
@@ -0,0 +1,20 @@
+# Difference between = and == operators in Python
+a = 10   # = is an assignment operator, assign value to a variable
+b = 20
+# == is a comparison operator, it checks if two variables are equal or not. Returns bool
+print(a == b)
+
+# ** operator in Python
+# It is a binary operator
+print(3 ** 2)   # ** is Exponentiation operator, 3^2
+
+# ^ operator in python
+# It is Bitwise XOR operator
+# It compares each bit of two numbers
+# It returns 1 if only one of the two bits is 1
+c = 6       # 6 = 110
+d = 3       # 3 = 001
+print(c ^ d)
+# the output will be 101 i.e. 5
+
+
---
+++ Task_10062024/Task02.py
@@ -0,0 +1,17 @@
+# Program to Calculate the square and cube of numbers
+
+num1 = 2
+print("Square of number is:", num1 ** 2)
+num2 = 8
+print("Cube of number is:", num2 ** 3)
+
+# Program to check whether a number is greater than, less than or equal to another number
+
+val1 = input("Enter first number:")
+val2 = input("Enter second number:")
+if(val1 > val2):
+    print("First number is greater than Second number")
+elif(val1 < val2):
+    print("Second number is greater than First number")
+else:
+    print("First number is equal to Second number")
